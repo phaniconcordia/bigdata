050332b9a9ef2edd92a44326b360c52e854b25d3
conted <conted@BH210-10.studentservers.JAC-SERVERS.JOHNABBOTT.QC.CA>
2018-05-26 13:38:31 -0400

    my lecture 1 notes

---
+++ lecture 1/Helloclass.java
@@ -0,0 +1,8 @@
+public class Helloclass {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+    public int printSum(int x,int y){
+        return x+y;
+    }
+}
